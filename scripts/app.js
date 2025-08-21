da9c5fb03ad3561982374cb9a857fe9fa0aa0a8b
HeidiLO <Osburn@s.dcsdk12.org>
2025-08-21 16:13:48 -0600

    do you like my button

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
 // This will be our first javascript file
+
+let mine = document.getElementById("My name");
+let output = document.getElementById("name");
+let myname = [
+    "this is my name",
+    "Whatever I want",
+    "This is super code",
+    "Blood on that bus"
+];
+
+let getRandomIndex = function (a) {
+    let rand = Math.random() * a.length;
+    return Math.floor(rand);
+};
+let getRandomWord = function (a) {
+    return a[getRandomIndex(a)];
+};
+let generateName = function (a, h, v, adj, n) {
+    let mynames =
+        getRandomWord(a) +
+        " "
+    console.log(mynames);
+    return mynames;
+};
+mine.addEventListener("click", function () {
+    let name = generateName(myname, "is");
+    output.textContent = name;
+});
