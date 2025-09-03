1a11255cec66cd504a7ad9df7d500b0370fb3324
Heidi <Osburn@s.dcsdk12.org>
2025-09-03 15:56:05 -0600

    soda pop

--- scripts/app.js
+++ scripts/app.js
@@ -93,30 +93,15 @@ console.log (profile);
 
 let k = document.getElementById("k-pop");
 let ks = [
- "Hey, hey, Hey hey, HeyDon't want you, need yo Yeah, I need you to fill me up 마시고 마셔 봐도 성에 차지 않아 Got a feeling that, oh, yeah
-You could be everything that
-That I need , taste so sweet 
-Every sip makes me want more, yeah"
-
-Don't want you, need you
-Yeah, I need you to fill me up
-마시고 마셔 봐도
-성에 차지 않아
-Got a feeling that, oh, yeah
-You could be everything that
-That I need , taste so sweet 
-Every sip makes me want more, yeah
-
-Lookin' like snacks 'cause you got it like that
-Take a big bite, want another bite, yeah
-너의 모든 걸 난 원해, 원해, 원해
-너 말곤 모두 뻔해, 뻔해, 뻔해
-When you're in my arms, I hold you so tight (So tight)
-Can't let go, no, no, not tonight
-
-지금 당장 날 봐 시간 없잖아
-넌 내꺼야 이미 알고 있잖아
-'Cause I need you to need me
-I'm empty, you feed me so refreshing
-My little soda pop"
+ "Hey, hey, Hey hey, Hey Don't want you, need yo Yeah, I need you to fill me up 마시고 마셔 봐도 성에 차지 않아 Got a feeling that, oh, yeah You could be everything that That I need , taste so sweet Every sip makes me want more, yeah Don't want you, need you Yeah, I need you to fill me up 마시고 마셔 봐도 성에 차지 않아 Got a feeling that, oh, yeah You could be everything that That I need , taste so sweet  Every sip makes me want more, yeah Lookin' like snacks 'cause you got it like that Take a big bite, want another bite, yeah 너의 모든 걸 난 원해, 원해, 원해 너 말곤 모두 뻔해, 뻔해, 뻔해 When you're in my arms, I hold you so tight (So tight) Can't let go, no, no, not tonight 지금 당장 날 봐 시간 없잖아 넌 내꺼야 이미 알고 있잖아 'Cause I need you to need me I'm empty, you feed me so refreshing My little soda pop You're all I can think of Every drop I drink up You're my soda pop My little soda pop Cool me down, you're so hot Pour me up, I won't stop You're my soda pop My little soda pop My little soda pop. Uh, make me wanna flip the top 한 모금에 you hit the spot Every little drip and drop, fizz and pop, ah 소름 돋아 it's gettin' hot Yes, I'm sippin' when it's drippin' now It's done? I need a second round And pour a lot and don't you stop 'Til my soda pop fizzles out 꿈 속에 그려왔던 너 난 절대 놓칠 수 없어 널 원해 꼭 I waited so long for a taste of soda So, the wait is over, baby Come and fill me up Just can't get enough Oh You're all I can think of Every drop I drink up You're my soda pop My little soda pop (Yeah, yeah) Cool me down, you're so hot Pour me up, I won't stop (Oh, oh) You're my soda pop My little soda pop Ooh, ooh Ooh, ooh You're my soda pop Gotta drink every drop"
 ];
+let generateK = function (a, ){
+    let mk =
+        getRandomWord(a) +
+    ""
+    return mk;
+};
+k.addEventListener("click", function () {
+    let ok = generateK(ks, );
+    output.textContent = ok;
+});
--- scripts/appp.js
+++ scripts/appp.js
@@ -1,5 +1,8 @@
+let two = 2;
+let three = 3; 
 if (2>3){
     console.log ("ahhh this is worong");
 } else{
     console.log ("I am a god");
 };
+
