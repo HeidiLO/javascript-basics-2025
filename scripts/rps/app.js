2a3458b7791cb865fac8ae6c99d937e07b4cdd8a
Heidi <Osburn@s.dcsdk12.org>
2025-09-08 16:24:02 -0600

    chesse

--- scripts/rps/app.js
+++ scripts/rps/app.js
@@ -3,20 +3,25 @@
 const ROCK = "rock";
 const PAPER ="paper ";
 const SCISSORS = "scissors";
-
-let omg = document.getElementById("Yes")
+const HAHAHA = "I win and you lose "
+let omg = document.getElementById("Yes");
 
 const shoot = function (weapon){
     console.log("player picked", weapon);
+
+    let computer = compWepon();
+    console.log ("I have picked",computer)
 };
 
 const compWepon = function (){
-    const rand = Math.floor(Math.random()*3);
+    const rand = Math.floor(Math.random()*4);
     if (rand == 0){
         return ROCK;
-    }
+    };
     if (rand == 1){
         return PAPER;
-    }
-    return SCISSORS;
+    };
+    if (rand == 2) {return SCISSORS;
+    };
+    return HAHAHA;
 };
