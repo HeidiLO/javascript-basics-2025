47cf45921e6bbf24864149c0d13001cbbf409d26
Heidi <Osburn@s.dcsdk12.org>
2025-09-09 16:23:25 -0600

    more stray kids

--- scripts/rps/app.js
+++ scripts/rps/app.js
@@ -25,3 +25,58 @@ const compWepon = function (){
     };
     return HAHAHA;
 };
+
+function answer(p,c){
+    let result = {
+        isTie: false,
+        playWin: false,
+        iWIn: false,
+        des: "",
+    };
+
+    if (p == c){
+        result.isTie = true;
+        result.des = "It's a tie?";
+        return result;
+    };
+    if (p == ROCK && c == SCISSORS){
+        result.playWin = true;
+        result.des = "It's a best out of three you have not won anything";
+        return result;
+    };
+        if (p == PAPER && c == SCISSORS){
+        result.iWIn = true;
+        result.des = "Look at meeeeee";
+        return result;
+    };
+       if (p == ROCK && c == PAPER){
+        result.iWIn = true;
+        result.des = "I told you I was better!!!!";
+        return result;
+    };
+        if (p == SCISSORS && c == PAPER){
+        result.playWin = true;
+        result.des = "You win I guess...?";
+        return result;
+    };
+       if (p == SCISSORS && c == ROCK){
+        result.iWin = true;
+        result.des = "Better luck next time..... or not better for me!!!";
+        return result;
+    };
+    if (p == SCISSORS && c == HAHAHA){
+        result.iWIn = true;
+        result.des = "Hahaha you suck";
+        return result;
+    };
+    if (p == ROCK && c == HAHAHA){
+        result.iWIn = true;
+        result.des = "Hahaha you suck";
+        return result;
+    };
+     if (p == PAPER && c == HAHAHA){
+        result.iWIn = true;
+        result.des = "Hahaha you suck";
+        return result;
+    };
+};
