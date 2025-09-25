826155acc6ff8004d8e64e4e75c7478e3b33be36
Heidi <Osburn@s.dcsdk12.org>
2025-09-25 16:22:57 -0600

    wow

---
+++ scripts/canvas.js
@@ -0,0 +1,59 @@
+//@ts-check
+
+/** @type { HTMLCanvasElement } */
+//@ts-ignore This is an HTML Canvas
+const CANVAS = document.getElementById("game-canvas");
+/**@type {CanvasRenderingContext2D} */
+//@ts-ignore
+const CTX = CANVAS.getContext("2d");
+const HEIGHT = 500;
+const WIDTH = 890;
+CANVAS.height = HEIGHT;
+CANVAS.width = WIDTH;
+CTX.fillStyle = "black";
+CTX.arc(0+ 300, 0 + 200, 3, 0, 2 * Math.PI);
+CTX.fill();
+let time = 0;
+let box = {
+    x: 0,
+    y: 0, 
+    xDir: 1,
+    yDir: 1,
+    width: 100,
+    height : 2,
+    draw: function(){
+    CTX.fillStyle = "pink"
+    CTX.fillRect (this.x,this.y,this.width, this.height)
+    CTX.fillStyle ="black"
+    CTX.fillRect (this.x+25,this.y,this.width - 50, this.height)
+    CTX.fillStyle = "pink"
+    CTX.fillRect (this.x +37.5, this.y, this.width - 75, this.height)  
+    },
+    update: function(){
+        let top = this.y;
+        let bottom = this.y + this.height;
+        let left = this.x;
+        let right = this.x + this.width;
+        if (top < 0){
+            this.yDir = 1;
+        } else if (bottom > HEIGHT){
+            this.yDir = - 1;
+        }else if (left < 0){
+            this.xDir = 1;
+        } else if (right> WIDTH){
+            this.xDir = - 1;
+        }
+
+        this.x += this.xDir;
+        this.y += this.yDir;
+    },
+};
+function drawloop(timestamp){
+     CTX.clearRect(0,0,WIDTH,HEIGHT)
+    let omgTime = timestamp - time;
+    time = timestamp;
+    box.draw();
+    box.update();
+    requestAnimationFrame(drawloop);
+};
+requestAnimationFrame(drawloop);
